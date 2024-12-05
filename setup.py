a680446684ca4bfebb1f3e2c75f8d2d691470b58
Armen-Jean Andreasian <armen_andreasian@proton.me>
2024-12-05 07:20:09 +0400

    Update setup.py (#5)
    Included pyyaml to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='not_gitmodules',
-    version='0.1.3',
+    version='0.1.3.1',
     packages=find_packages(),
     license='Custom License',
     entry_points={
@@ -10,6 +10,9 @@ setup(
             'not_gitmodules=not_gitmodules.cli:cli',
         ],
     },
+    install_requires=[
+        'pyyaml',
+    ],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Armen-Jean Andreasian',
