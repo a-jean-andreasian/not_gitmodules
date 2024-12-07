d61974e1fb2e617d2ba13cd204bc97689284743d
Armen-Jean Andreasian <armen_andreasian@proton.me>
2024-12-07 07:17:53 +0700

    fixed fnf error content

--- not_gitmodules/parts/yaml_reader.py
+++ not_gitmodules/parts/yaml_reader.py
@@ -5,7 +5,7 @@ import os
 def read_yaml(file_path):
     """Read and parse the YAML file."""
     if not os.path.exists(file_path):
-        raise FileNotFoundError(f"{os.path.abspath(file_path)} was not found.")
+        raise FileNotFoundError(f"No yaml file was found at {os.path.abspath(file_path)} make sure you have it.")
     else:
         with open(file_path, "r") as file:
             return yaml.safe_load(file)
